cbeeced81daefbdb971019d528c1f2f8765fd241
Henry Abel <promisekept836@outlook.com>
2022-08-30 15:41:16 -0400

    setting up redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import MainView from "./components/main-view/main-view";
-import Container from 'react-bootstrap/Container'
-
+import Container from "react-bootstrap/Container";
+import Provider from "react-redux";
+import store from "./store";
 
 function App() {
   return (
     <Container>
+      {/* <Provider store={store}> */}
       <MainView />
+      {/* </Provider> */}
     </Container>
   );
 }
